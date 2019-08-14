84f16815b40fe55866f18c676d2d6a3be517b602
Hector Rodriguez Duenas <h.rodri831@gmail.com>
2019-08-14 18:29:35 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,7 +15,10 @@ def
   if phrase = "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
   end
-    elsif  phrase = phrase.upcase
+  elsif  phrase = phrase.upcase
     puts "NO, NOT SINCE 1938!"
   end 
+else 
+  puts "HUH?! SPEAK UP, SONNY!"
+end
 end 
